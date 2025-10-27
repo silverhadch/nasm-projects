cee3ab4530aaedc84a9004fb0b61667516470015
Hadi Chokr <hadichokr@icloud.com>
2025-10-27 13:44:02 +0100

    Use ret

--- src/c-caller/caller.asm
+++ src/c-caller/caller.asm
@@ -1,18 +1,22 @@
+; caller.asm - Calls a C library function (puts) from assembly
+; Global declarations for linker
 global main
+; External declaration for puts function
+extern puts
+
+; Assemble and link with:
 ; nasm -felf64 caller.asm && gcc -no-pie caller.o -o caller && ./caller
+
+; Text section
 section .text
+; Program entry point
+; C Main will under the hood call _start
 main:
     ; Call the input.asm program
-    extern puts
-    mov rdi, msg
-    call puts
-    ; ret removed to have a null status exit
-    ; because the return value of puts is the length of the string
+    mov rdi, msg   ; Load address of message into rdi
+    call puts      ; Call the puts function from C library
+    xor eax, eax   ; Return 0 by setting eax to 0 by comparing it with itself if equal set zero flag
+    ret            ; Return from main
 
-    ; Strings in C end with null terminator
-    mov eax, 0x01
-    mov ebx, 0
-    int 0x80
-
+section .data
 msg db "Calling input.asm program...", 0
-
